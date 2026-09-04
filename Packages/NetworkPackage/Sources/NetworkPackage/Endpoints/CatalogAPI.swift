b46b2a6729c201fac60f63434875f2e7a136997b
Valeriy Solovey <sye7qjm3ac@icloud.com>
2026-09-04 19:01:02 +0700

    feat(network): define CategoryDTO and implement CatalogAPI

---
+++ Packages/NetworkPackage/Sources/NetworkPackage/Endpoints/CatalogAPI.swift
@@ -0,0 +1,42 @@
+//
+//  CatalogAPI.swift
+//  NetworkPackage
+//
+//  Created by Valeriy Solovey on 04.09.2026.
+//
+
+public struct CatalogAPI: CatalogAPIProtocol {
+    private let apiClient: APIClient
+
+    public init(apiClient: APIClient) {
+        self.apiClient = apiClient
+    }
+
+    public func fetchCategories() async throws -> [CategoryDTO] {
+        let output = try await apiClient.client.getCategories(.init())
+
+        switch output {
+        case .ok(let response):
+            let categories = try response.body.json
+
+            return categories.map {
+                CategoryDTO(
+                    id: $0.id,
+                    name: $0.name,
+                    image: $0.image
+                )
+            }
+
+        case .unauthorized(let response):
+            let error = try response.body.json
+            throw APIError.unauthorized(message: error.error)
+
+        case .default(statusCode: let statusCode, let response):
+            let error = try response.body.json
+            throw APIError.server(
+                statusCode: statusCode,
+                message: error.error
+            )
+        }
+    }
+}
