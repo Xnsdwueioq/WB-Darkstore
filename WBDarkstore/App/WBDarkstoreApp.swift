cb5dab6f3e4d22d78b6161db0a614d5d90cb70c5
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-09-04 16:16:16 +0700

    feat(UI): add icon and launch screen

---
+++ WBDarkstore/App/WBDarkstoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBDarkstoreApp.swift
+//  WBDarkstore
+//
+//  Created by sye7qjm3ac on 04.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct WBDarkstoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WBDarkstore/Screens/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WBDarkstore
+//
+//  Created by sye7qjm3ac on 04.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ContentView")
+    }
+}
+
+#Preview {
+    ContentView()
+}
