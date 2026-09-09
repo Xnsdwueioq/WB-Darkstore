a4f0b7f2fc03be2e4e1fe9a404cf3c1869eb57c3
ermak3705 <ermakov.ilia3705@gmail.com>
2026-09-09 20:08:02 +0300

    Перенесес в ветку бизнес логики  CompositionRoot и WBDarkstoreApp из feature/categories, переименовал папка ViewModel в Model

---
+++ WBDarkstore/App/CompositionRoot.swift
@@ -0,0 +1,35 @@
+//
+//  CompositionRoot.swift
+//  WBDarkstore
+//
+//  Created by Valeriy Solovey on 04.09.2026.
+//
+
+import BusinessLogic
+import NetworkPackage
+
+struct CompositionRoot {
+    let catalogService: CatalogService
+    let productService: ProductService
+    let cartService: CartService
+    let orderService: OrderService
+    let addressService: AddressService
+    let profileService: ProfileService
+
+    init(token: String) throws {
+        let apiClient = try APIClient(token: token)
+        let catalogAPI = CatalogAPI(apiClient: apiClient)
+        let productAPI = ProductAPI(apiClient: apiClient)
+        let cartAPI = CartAPI(apiClient: apiClient)
+        let orderAPI = OrderAPI(apiClient: apiClient)
+        let addressAPI = AddressAPI(apiClient: apiClient)
+        let profileAPI = ProfileAPI(apiClient: apiClient)
+
+        catalogService = CatalogService(api: catalogAPI)
+        productService = ProductService(productAPI: productAPI)
+        cartService = CartService(cartAPI: cartAPI)
+        orderService = OrderService(orderAPI: orderAPI)
+        addressService = AddressService(addressAPI: addressAPI)
+        profileService = ProfileService(profileAPI: profileAPI)
+    }
+}
