0aa17e4766ef9e7dbcc4de7b94f75e9f0d62db98
Valeriy Solovey <sye7qjm3ac@icloud.com>
2026-09-14 20:48:09 +0700

    build(dev): add swiftlint and update its rules

--- WBShop/Core/Package.swift
+++ WBShop/Core/Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "Core",
     platforms: [
         .iOS(.v18),
+        .macOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -15,11 +16,23 @@ let package = Package(
             targets: ["Core"]
         ),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/realm/SwiftLint",
+            from: "0.65.1"
+        )
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Core"
+            name: "Core",
+            plugins: [
+                .plugin(
+                    name: "SwiftLintBuildToolPlugin",
+                    package: "SwiftLint"
+                )
+            ]
         ),
 
     ],
--- WBShop/DSKit/Package.swift
+++ WBShop/DSKit/Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "DSKit",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v18),
+        .macOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -15,6 +16,12 @@ let package = Package(
             targets: ["DSKit"]
         ),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/realm/SwiftLint",
+            from: "0.65.1"
+        )
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -22,6 +29,12 @@ let package = Package(
             name: "DSKit",
             resources: [
                 .process("Resources")
+            ],
+            plugins: [
+                .plugin(
+                    name: "SwiftLintBuildToolPlugin",
+                    package: "SwiftLint"
+                )
             ]
         ),
 
